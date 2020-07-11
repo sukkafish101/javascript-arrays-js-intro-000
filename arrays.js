f629418a7f14c11a541dced25dcbb2dcae3e7176
Brandon Lagrone <sukkafish101@hotmail.com>
2020-07-11 20:25:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ['snicker', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  return (element, ...array)
+}
+
+function destructivelyAddElemtnToBeginningOfArray(array, element){
+  array.unshift(element)
+}
