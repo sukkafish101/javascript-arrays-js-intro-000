3bd37c835e6362106efd5708c42d1c0154126ae4
Brandon Lagrone <sukkafish101@hotmail.com>
2020-07-11 20:44:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  return (...array, element)
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
